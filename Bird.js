21b1548a2506147c427ad0c0426e97481bbd3416
varunAditya-28 <varunaditya.pds@gmail.com>
2021-06-04 13:16:30 +0530

    trajectory

--- Bird.js
+++ Bird.js
@@ -1,13 +1,30 @@
 class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
+    this.trajectory=[]
     this.image = loadImage("sprites/bird.png");
+    this.smokeImg = loadImage("sprites/smoke.png")
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
-
+3
     super.display();
+
+    if(this.body.velocity.x>10 && this.body.position.x > 200){
+    var position = [this.body.position.x,this.body.position.y];
+    this.trajectory.push(position)
+    }  
+
+    for(var i=0;i<this.trajectory.length;i++){
+    image(this.smokeImg,this.trajectory[i][0],this.trajectory[i][1]) 
+    }
+
   }
+
+   clearSmoke(){
+   this.trajectory = []
+   }
+
 }
